098db2ff1abf43576a6ee1013322a250531a72f1
Matthew Perkins <mfperkins@gmail.com>
2016-09-30 15:46:46 +0100

    done

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -1,7 +1,8 @@
 var sports = "https://spy-api.herokuapp.com/apis?api-key=b202a9029f8b54415a7cb0e2e775536c00920833&json=AllSport";
-var politics = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=politics?show-fields=body";
-var business = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=business?show-fields=body";
-var technology = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=technology?show-fields=body";
+var politics = "https://spy-api.herokuapp.com/apis?api-key=b202a9029f8b54415a7cb0e2e775536c00920833&json=AllSport";
+// var politics = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=politics?show-fields=body";
+// var business = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=business?show-fields=body";
+// var technology = "http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?q=technology?show-fields=body";
 
 storyFactory = new StoryFactory(Story);
 
