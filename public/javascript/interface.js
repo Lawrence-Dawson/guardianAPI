8e9fdbb1ed16b09255b18f9b75ae42d6afd43440
supasuma <supasuma@gmail.com>
2016-09-30 15:11:04 +0100

    fixing merge conflict

--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -15,8 +15,7 @@ function printStory(id, story) {
   prettyPrinter.createLink(id, story.url);
 }
 
-// var sports = "https://spy-api.herokuapp.com/apis?api-key=b202a9029f8b54415a7cb0e2e775536c00920833&json=AllSport";
-var sports = "https://content.guardianapis.com/search?q=sports?show-fields=body&api-key=8c51e780-17f2-4cb6-9771-5e29548d62be";
+var sports = "https://spy-api.herokuapp.com/apis?api-key=b202a9029f8b54415a7cb0e2e775536c00920833&json=AllSport";
 
 
 function getAllStories(url) {
@@ -25,7 +24,6 @@ function getAllStories(url) {
      storiesURLs.forEach(function(url) {
        getStories(url).then(function(response){
          var story = storyFactory.parse(response);
-         console.log(story);
          printStory(Math.random(), story);
   });
 });
